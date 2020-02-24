56776a297b95af56ae362e2f3a9e887cbfa407c3
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-02-24 22:33:12 +0300

    Debouncing Add Event

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
+const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
+
+const start = debounce(() => {
+  console.log('STARTING USERS PROGRAM!');
+}, 100);
 
 chokidar
   .watch('.')
-  .on('add', () => console.log('File Added!'))
+  .on('add', start)
   .on('change', () => console.log('File Changed!'))
   .on('unlink', () => console.log('File Unlinked!'));
